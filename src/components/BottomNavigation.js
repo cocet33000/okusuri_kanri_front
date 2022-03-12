ae8cb01f6a74151a05458ce5138d05c655774773
takumi okamoto <takumiokamoto@MacBook-Pro.local>
2022-03-12 19:42:35 +0900

    下部に固定

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -4,11 +4,13 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Paper from '@mui/material/Paper';
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
 
   return (
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
     <BottomNavigation
     showLabels
     value={value}
@@ -20,5 +22,6 @@ export default function SimpleBottomNavigation() {
     <BottomNavigationAction label="Fuga" icon={<FavoriteIcon />} />
     <BottomNavigationAction label="Piyo" icon={<LocationOnIcon />} />
     </BottomNavigation>
+    </Paper>
   );
 }
